1a08ba8c398928fae274e52b6698987a57297d26
Frable <bfrable@Buffalos-MacBook-Pro-2.local>
2014-12-15 19:30:48 -0500

    add menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,29 @@
-function progress(percent, $element) {
-    var progressBarWidth = percent * $element.width() / 100;
-    $element.find('div').animate({ width: progressBarWidth }, 500);
-    $element.append('<span class="percent-text">' + percent + "%</span>");
-}
+jQuery(function ($) {
 
-progress(20, $('#progressBar'));
+    var helpers = {
+
+        Elements: {
+            progressEl: $("#progressBar"),
+            content: $("#content"),
+        },
+
+        progressBar: function() {
+			function progress(percent, $element) {
+			    var progressBarWidth = percent * $element.width() / 100;
+			    $element.find('div').animate({ width: progressBarWidth }, 500);
+			    $element.append('<span class="percent-text">' + percent + "%</span>");
+			}
+
+			progress(20, helpers.Elements.progressEl);            
+        },
+
+        init: function() {
+            helpers.progressBar();
+        }
+
+    };
+
+    // Initialize on DOM ready
+    $(helpers.init);
+
+});
